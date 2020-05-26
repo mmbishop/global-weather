a969547a124fedd594a12202ec71db0729fe0afb
Mike Bishop <tend2dv8@gmail.com>
2020-05-26 10:34:43 -0500

    Replaced the hardcoded API key with a reference to the environment variable.

--- src/services/weather/index.js
+++ src/services/weather/index.js
@@ -1,4 +1,5 @@
-export { default as getWeather} from './get';
+export { default as getWeather } from './get';
+export { default as getForecast } from './forecast';
+export { default as getWeatherMap } from './map';
 export { convertTemperature } from './converters';
 export { convertSpeed } from './converters';
-export { default as getForecast} from './forecast';
--- src/services/weather/map.js
+++ src/services/weather/map.js
@@ -1,3 +1,4 @@
-export const getWeatherMap = (lat, lng) => {
-    window.fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&APPID=4a072a2d89070b207876f6f8a46ed21f`)
-}
+export default (lat, lng) => (
+    window.fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&APPID=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
+        .then(res => res.json)
+);
