47dd26c018017676f9f9c52475db7bb7738ebdac
Mike Bishop <tend2dv8@gmail.com>
2020-05-27 17:10:39 -0500

    Changed map layer to Google Maps.

--- src/components/WeatherMap/WeatherMap.js
+++ src/components/WeatherMap/WeatherMap.js
@@ -18,10 +18,10 @@ const WeatherMap = ({show, placeName, adminLevel1, country, lat, lng, onClose}) 
             </ModalHeader>
             <ModalBody>
                 <Map id={"weather-map"} center={position} zoom={11}>
-                    <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-                               attribution={"&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"}/>
-                    <TileLayer url={`https://{s}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`}
-                               opacity={0.6} attribution={"Weather from OpenWeatherMap"}/>
+                    <TileLayer url={`https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&key=${process.env.REACT_APP_GOOGLE_MAPS_CLIENT_ID}`}
+                               attribution={"&copy; Google Maps"}/>
+                    <TileLayer url={`https://{s}.tile.openweathermap.org/map/precipitation_cls/{z}/{x}/{y}.png?appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`}
+                               opacity={0.6} attribution={"&copy; OpenWeatherMap"}/>
                     <Legend/>
                 </Map>
             </ModalBody>
