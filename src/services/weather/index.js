3067b12d9e8dff04f92e42362688925e4eee9be6
Mike Bishop <tend2dv8@gmail.com>
2020-05-27 15:42:02 -0500

    map.js is no longer needed.

--- src/services/weather/index.js
+++ src/services/weather/index.js
@@ -1,5 +1,4 @@
 export { default as getWeather } from './get';
 export { default as getForecast } from './forecast';
-export { default as getWeatherMap } from './map';
 export { convertTemperature } from './converters';
 export { convertSpeed } from './converters';
--- src/services/weather/map.js
+++
@@ -1,4 +0,0 @@
-export default (lat, lng) => (
-    window.fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&APPID=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
-        .then(res => res.json)
-);
